c4d0c666685e86a30267aabd37db8c294eea8e87
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2021-02-14 11:46:37 -0400

    Atualizando

---
+++ script.js
@@ -0,0 +1,12 @@
+let lista = new Vue({
+    el:'#lista',
+    data:{
+        nomes:[
+            {nome:'thiago', idade:'32'},
+            {nome:'thiago1', idade:'32'},
+            {nome:'thiago2', idade:'32'},
+            {nome:'thiago3', idade:'32'},
+            {nome:'thiago4', idade:'32'},
+        ]
+    }
+});
