eef2a27beadab093c7475be0dbf0d8228ce13108
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2021-02-14 17:05:14 -0400

    Ataulizando

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function bandeiras(pais)
+{
+    return "<span> Bandeira do(a) "+ pais +"</span>";
+}
+
+Vue.component('brasil', {
+    template: "<p>Algo!!</p>"
+});
+Vue.component('pais', {
+    props:['nome'],
+    template: "<p v-html='nome'></p>"
+});
+
 let lista = new Vue({
     el:'#lista',
     data:{
@@ -7,6 +20,33 @@ let lista = new Vue({
             {nome:'thiago2', idade:'32'},
             {nome:'thiago3', idade:'32'},
             {nome:'thiago4', idade:'32'},
-        ]
+        ],
+        pais: '',
+        argentina: bandeiras('argentina'),
+        brasil: bandeiras('brasil'),
+        chile: bandeiras('chile'),
+        n1: Math.floor(Math.random()*50),
+        n2: Math.floor(Math.random()*50),
+        n3: 0,
+        nome: "thiago"
+    },
+    methods: {
+        mostrar: (nome) => {
+            let txt = 'Olá, ' + nome;
+            return txt;
+        },
+        testar: ()=> {
+            return 'Teste 1, 2, 3...';
+        },
+        paises: function() {
+            return [
+                {bandeira: this.argentina , continente: 'América do Sul'},
+                {bandeira: this.brasil, continente: 'América do Sul'},
+                {bandeira: this.chile , continente: 'América do Sul'}
+            ];
+        }
+
     }
 });
+
+lista.pais = 'brasil';
